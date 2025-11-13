7d6330f7ca0df1d1abdcb768e18e094df542d9a1
Gabriel <bielbuttendorf@gmail.com>
2025-11-13 06:18:23 -0300

    validação com react-hook-form e zod

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -30,7 +30,12 @@ export const SearchFormContainer = styled.form`
     border-radius: 6px;
     cursor: pointer;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.5;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${(props) => props.theme['purple-500']};
       border: 1px solid ${(props) => props.theme['purple-500']};
       color: ${(props) => props.theme['white']};
